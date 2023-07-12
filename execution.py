e3acef2efa05aa08deffd7e416110d7b9c56d345
victorgonl <victorgonl@outlook.com>
2023-07-12 12:18:43 -0300

    Não salvar dados quando executar os experimentos

--- execution.py
+++ execution.py
@@ -1,4 +1,3 @@
-from src.functions.plot_functions import plotar_gráfico, plotar_gráfico_de_dispersão, plotar_itens
 from src.knapsack_problem.item import gerar_itens_aleatórios
 from src.knapsack_problem.knapsack import avaliar_mochila, diferença_entre_mochilas, gerar_mochila_aleatória, gerar_mochila_vizinha
 from src.simulated_annealing.parallel_simulated_annealing import parallel_simulated_annealing
@@ -11,9 +10,13 @@ import os
 import cpuinfo
 
 
+to_save = False
+to_save_population = to_save
+show_progress_bar = not to_save
+
 date = datetime.datetime.now().strftime("%Y%m%d-%H%M")
 directory = f"./data/{date}/"
-os.makedirs(directory, exist_ok=True)
+os.makedirs(directory, exist_ok=True) if to_save else None
 
 print()
 print("Experimentação:", date)
@@ -23,7 +26,7 @@ print()
 # ============================== POPULAÇÃO ============================== #
 
 # parâmetros da população
-parâmetros_da_população = {"número_de_itens": 1000000,
+parâmetros_da_população = {"número_de_itens": 100000,
                             "valor_mínimo" :0.0,
                             "valor_máximo": 1.0,
                             "peso_mínimo": 0.0,
@@ -42,12 +45,13 @@ itens_dict = {"id": [item.id for item in itens],
               "valor": [item.valor for item in itens],
               "peso": [item.peso for item in itens]}
 
-with open(f"{directory}pop_param.json", "w") as outfile:
-    json.dump(parâmetros_da_população, outfile, indent=4)
+if to_save:
+    with open(f"{directory}pop_param.json", "w") as outfile:
+        json.dump(parâmetros_da_população, outfile, indent=4)
 
-with open(f"{directory}pop.json", "w") as outfile:
-    json.dump(itens_dict, outfile, indent=4)
-
+if to_save and to_save_population:
+    with open(f"{directory}pop.json", "w") as outfile:
+        json.dump(itens_dict, outfile, indent=4)
 
 
 # ============================== SOLUÇÃO INICIAL ============================== #
@@ -63,24 +67,26 @@ mochila_inicial = gerar_mochila_aleatória(capacidade=capacidade_das_mochilas,
 mochila_inicial_dict = {"capacidade": mochila_inicial.capacidade,
                         "itens": [item.id for item in mochila_inicial.itens]}
 
-with open(f"{directory}init_solution.json", "w") as outfile:
-    json.dump(mochila_inicial_dict, outfile, indent=4)
+if to_save:
+    with open(f"{directory}init_solution.json", "w") as outfile:
+        json.dump(mochila_inicial_dict, outfile, indent=4)
 
 
 # ============================== MÁQUINA ============================== #
 
 # parâmetros da experimentação
-parâmetros_da_experimentação = {"número_de_execuções": 100,
+parâmetros_da_experimentação = {"número_de_execuções": 1,
                                 "processes_number": [1, 2, 4, 8, 16]}
-
-with open(f"{directory}exp_params.json", "w") as outfile:
-    json.dump(parâmetros_da_experimentação, outfile, indent=4)
 
 # parâmetros da(s) máquina(s)
 cpu_info = cpuinfo.get_cpu_info()
 
-with open(f"{directory}cpu_info.json", "w") as outfile:
-    json.dump(cpu_info, outfile, indent=4)
+if to_save:
+    with open(f"{directory}exp_params.json", "w") as outfile:
+        json.dump(parâmetros_da_experimentação, outfile, indent=4)
+
+    with open(f"{directory}cpu_info.json", "w") as outfile:
+        json.dump(cpu_info, outfile, indent=4)
 
 
 # ============================== ALGORITMOS ============================== #
@@ -89,10 +95,12 @@ with open(f"{directory}cpu_info.json", "w") as outfile:
 parâmetros_do_algoritmo = {"temperatura_inicial": 1,
                            "temperatura_final": 0.1,
                            "taxa_de_resfriamento": 0.01,
-                           "número_de_vizinhos_a_explorar": 1000}
+                           "número_de_vizinhos_a_explorar": 1000,
+                           "forma_de_seleção": "optimal"}
 
-with open(f"{directory}algoritm_params.json", "w") as outfile:
-    json.dump(parâmetros_do_algoritmo, outfile, indent=4)
+if to_save:
+    with open(f"{directory}algoritm_params.json", "w") as outfile:
+        json.dump(parâmetros_do_algoritmo, outfile, indent=4)
 
 """ # execução do algoritmo sequencial
 for i in range(parâmetros_da_experimentação["número_de_execuções"]):
@@ -131,7 +139,6 @@ for i in range(parâmetros_da_experimentação["número_de_execuções"]):
 
     print() """
 
-
 # execução do algoritmo paralelo
 k = 0
 
@@ -148,18 +155,20 @@ for k in range(len(parâmetros_da_experimentação["processes_number"])):
             temperatura_final=parâmetros_do_algoritmo["temperatura_final"],
             taxa_de_resfriamento=parâmetros_do_algoritmo["taxa_de_resfriamento"],
             número_de_vizinhos_a_explorar=parâmetros_do_algoritmo["número_de_vizinhos_a_explorar"],
+            forma_de_seleção=parâmetros_do_algoritmo["forma_de_seleção"],
             gerar_solução_vizinha=gerar_mochila_vizinha,
             comparar_soluções=diferença_entre_mochilas,
             avaliar_solução=avaliar_mochila,
             função_de_resfriamento=geometric_reduction,
             solução_inicial=mochila_inicial,
             itens=itens,
-            número_de_processos=número_de_processos
+            número_de_processos=número_de_processos,
+            mostrar_barra_de_progresso=show_progress_bar
         )
 
         print("Tempo de execução: %s seconds" % registro["run_time"])
         print("Total de itens na mochila:", len(solução.itens))
-        print("Itens na mochila:", [item.id for item in solução.itens])
+        #print("Itens na mochila:", [item.id for item in solução.itens])
         print("Valor total na mochila:", avaliar_mochila(solução))
         print("Peso total na mochila:", solução.peso())
         print("Número de vizinhos explorados:", registro["número_vizinhos_explorados"])
@@ -172,7 +181,8 @@ for k in range(len(parâmetros_da_experimentação["processes_number"])):
                         "itens": [item.id for item in solução.itens],
                         "solution": [[item.id for item in solução.itens] for solução in registro["solução"]]}
 
-        with open(f"{directory}run-PSA{número_de_processos}-{i}.json", "w") as outfile:
-            json.dump(registro_dict, outfile, indent=4)
+        if to_save:
+            with open(f"{directory}run-PSA{número_de_processos}-{i}.json", "w") as outfile :
+                json.dump(registro_dict, outfile, indent=4)
 
         print()
